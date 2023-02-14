24a3e5355d1acf9b8a8b4ea5a9ee1bec31b9f356
Lubo Furinda <l.furinda@gmail.com>
2023-02-14 11:30:53 +0100

    Added user context and functionality with sign in/sign out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,10 @@
-import {useState} from 'react';
+import {useState, useContext} from 'react';
 
 import FormInput from "../form-input/form-input.component"
 import Button from '../button/button.component';
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from '../../contexts/user.context';
 
 import './sign-in.styles.scss'
 
@@ -17,6 +19,8 @@ const SignInForm = () => {
 
     const [formFields, setFormFields] = useState(defaultformFields)
     const { email, password } = formFields;
+
+    const { setCurrentUser } = useContext(UserContext)
 
     const resetFormFields = () => {
         setFormFields(defaultformFields)
@@ -37,7 +41,8 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-           await signInAuthUserWithEmailAndPassword(email, password)
+           const {user} = await signInAuthUserWithEmailAndPassword(email, password)
+           setCurrentUser(user)
            resetFormFields();
         } catch (error) {
             switch(error.code) {
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import { getAuth,
   GoogleAuthProvider, 
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword, 
+  signOut,
 } from 'firebase/auth'
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
@@ -73,3 +74,5 @@ const firebaseConfig = {
     return await signInWithEmailAndPassword(auth, email, password)
 
   }
+
+  export const signOutUser = async () => await signOut(auth);
